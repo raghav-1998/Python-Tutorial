69873753aa759da56453117c120af80de4ae8ee9
raghav-1998 <raghav.vasdeva@gmail.com>
2023-04-11 00:19:54 +0530

    Adding Demo of Nested If statement

---
+++ Control Structures/Nested-If-Demo.py
@@ -0,0 +1,19 @@
+num1=10
+num2=20
+num3=30
+if(num1>num2):
+    if(num1>num3):
+        print("num1 is greater")
+    else:
+        print("num3 is greater")
+elif(num2>num3):
+    print("num2 is greater")
+else:
+    print("num3 is greater")
+
+
+"""
+Output:
+
+num3 is greater
+"""
