4d738ec6a19529c37d1fb662afac4814c2aa01d8
raghav-1998 <raghav.vasdeva@gmail.com>
2023-04-10 14:52:13 +0530

    Adding Demos of Variable & DataType

---
+++ Basics of Python/Demo1.py
@@ -0,0 +1,45 @@
+num1=123 #Decalaring variable
+print(num1) #printing value of variable
+print(type(num1)) #Printing type of variable
+
+num2=1237126381763817
+print(num2)
+print(type(num2))
+
+num3=123.45
+print(num3)
+print(type(num3))
+
+num4=123123.32345324
+print(num4)
+print(type(num4))
+
+char1='A'
+print(char1)
+print(type(char1))
+
+string1="Hello World"
+print(string1)
+print(type(string1))
+
+flag1=True
+print(flag1)
+print(type(flag1))
+
+""""
+Output:
+123
+<class 'int'>
+1237126381763817
+<class 'int'>
+123.45
+<class 'float'>
+123123.32345324
+<class 'float'>
+A
+<class 'str'>
+Hello World
+<class 'str'>
+True
+<class 'bool'>
+"""
