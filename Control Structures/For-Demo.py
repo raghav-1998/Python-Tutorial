c74c4712ab56e9fba2cfe6f30d7b5e92c8bae932
raghav-1998 <raghav.vasdeva@gmail.com>
2023-04-11 19:07:29 +0530

    Adding Demos of For loop

---
+++ Control Structures/For-Demo.py
@@ -0,0 +1,51 @@
+print("\n")
+print("<------ 1st variation ------>")
+for number in 1,2,3,4,5:
+    print("The current number is ",number)
+
+print("\n")
+print("<------ 2nd variation ---->")
+"""
+Output:
+
+The current number is  1
+The current number is  2
+The current number is  3
+The current number is  4
+The current number is  5
+"""
+
+start=1
+end=10
+step=2
+for number in range (start, end, step):
+    print("The current number is ", number)
+
+
+print("\n")
+print("<-----3rd Variation ------>")
+"""
+Output:
+
+The current number is  1
+The current number is  3
+The current number is  5
+The current number is  7
+The current number is  9
+
+"""
+
+for number in range(5,0,-1):
+    print ("The current number is ",number)
+
+"""
+Output:
+
+The current number is 5
+The current number is 4
+The current number is 3
+The current number is 2
+The current number is 1
+
+"""
+
