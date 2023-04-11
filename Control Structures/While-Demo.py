628e5176e9ba10ed4126089e42602132fc992123
raghav-1998 <raghav.vasdeva@gmail.com>
2023-04-11 19:06:39 +0530

    Adding Demo of While loop

---
+++ Control Structures/While-Demo.py
@@ -0,0 +1,18 @@
+num=5
+count=1
+while count <= num:
+    print("the current number is:",count)
+    count+=1
+
+
+
+"""
+Output:
+
+the current number is: 1
+the current number is: 2
+the current number is: 3
+the current number is: 4
+the current number is: 5
+
+"""
